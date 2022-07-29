6d2556f42192ef1ab951da111b074ffe16808712
Oleguis <anjevvjl@gmail.com>
2022-07-28 22:20:59 -0500

    Agregando stylos responsivos

--- client/src/App.js
+++ client/src/App.js
@@ -6,28 +6,12 @@ function App () {
   const [listaTextos, setListaTextos] = useState([])
 
   return (
-    <div className='text-center' style={styles.divPpal}>
-      <div style={styles.divSecondary}>
+    <div className='text-center' style={{ backgroundColor: 'lightgray', height: '98vh' }}>
+      <div>
         <FormGetText listaTextos={listaTextos} setListaTextos={setListaTextos} />
         <ListResults listaTextos={listaTextos} />
       </div>
     </div>
   )
 }
-
-// Estilos css en línea para los divs
-const styles = {
-  divPpal: {
-    display: 'flex',
-    height: '100vh',
-    justifyContent: 'center',
-    alignItems: 'center'
-  },
-  divSecondary: {
-    backgroundColor: '#e7e9eb',
-    width: '95%',
-    height: '95vh'
-  }
-}
-
 export default App
--- client/src/components/ListResults.js
+++ client/src/components/ListResults.js
@@ -2,26 +2,25 @@ import React from 'react'
 
 function ListResults ({ listaTextos }) {
   return (
-    <div className='
-      container
-      m-5
-      p-5
-      bg-light
-      text-start
-      d-flex
-      justify-content-center
-      w-100'
-    >
-      <h5 className='text-start'>Resultado:</h5>
-      <ul className='list-group w-100 d-flex align-items-center h-100'>
-        {listaTextos.map((texto, pos) => {
-          return (
-            <li className='list-group-item m-2 w-75 border border-secondary text-start rounded' key={texto + pos}>
-              {texto}
-            </li>
-          )
-        })}
-      </ul>
+    <div class='container text-center bg-light my-3 py-2 w-75 rounded'>
+      <div class='row'>
+        <div class='col-sm'>
+          <h5 className='text-start'>Resultado:</h5>
+        </div>
+      </div>
+      <div class='row'>
+        <div class='col-sm'>
+          <ul className='list-group d-flex align-items-center'>
+            {listaTextos.map((texto, pos) => {
+              return (
+                <li className='list-group-item m-2 w-75 border border-secondary text-start rounded' key={texto + pos}>
+                  {texto}
+                </li>
+              )
+            })}
+          </ul>
+        </div>
+      </div>
     </div>
   )
 }
